c3dbeedcf9ec090e90014ef8e7ee5f5a99b21ced
Aaron Holmes <aaron@aaronholmes.net>
2016-01-29 13:51:16 -0800

    Fix formatting.

--- ip/Startup.cs
+++ ip/Startup.cs
@@ -5,13 +5,13 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace ip
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
@@ -32,5 +32,5 @@ namespace ip
 
         // Entry point for the application.
         public static void Main(string[] args) => WebApplication.Run<Startup>(args);
-    }
+	}
 }
